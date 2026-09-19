0fb9e915b999a3217cb97d501719822449d4ee8a
Delta <delta@deltawolf.net>
2026-09-19 08:03:20 -0700

    Add option to use the header bar as the collapse button
    Resolves #1

--- InspectorImprovements/InspectorImprovements.cs
+++ InspectorImprovements/InspectorImprovements.cs
@@ -16,18 +16,17 @@ public class InspectorImprovements : ResoniteMod {
 	public override string Version => VERSION_CONSTANT;
 	public override string Link => "https://github.com/XDelta/Inspectorimprovements";
 
-	//[AutoRegisterConfigKey]
-	//private static readonly ModConfigurationKey<bool> enabled = new("enabled", "", () => true);
+	[AutoRegisterConfigKey]
+	private static readonly ModConfigurationKey<bool> collapseComponents = new("collapseComponents", "Enable the collapse functionality on components", () => true);
 
 	[AutoRegisterConfigKey]
-	private static readonly ModConfigurationKey<bool> collapseComponents = new("collapseComponents", "Collapse button on components", () => true);
-
-	[AutoRegisterConfigKey]
-	private static readonly ModConfigurationKey<bool> defaultExpanded = new("defaultExpanded", "Expanded by default", () => true);
+	private static readonly ModConfigurationKey<bool> defaultExpanded = new("defaultExpanded", "Expanded components by default", () => true);
 
 	[AutoRegisterConfigKey]
 	private static readonly ModConfigurationKey<long> collaspeOrderOffset = new("collaspeOrderOffset", "Collaspe button OrderOffset", () => -1);
 
+	[AutoRegisterConfigKey]
+	private static readonly ModConfigurationKey<CollapseStyle> collapseStyle = new("collapseStyle", "How should collapsing components be styled (button with icon or full header)", () => CollapseStyle.Line);
 	internal static ModConfiguration? Config;
 
 	public override void OnEngineInit() {
@@ -38,6 +37,10 @@ public class InspectorImprovements : ResoniteMod {
 		harmony.PatchAll();
 	}
 
+	enum CollapseStyle {
+		Line,         // |--|
+		Header       // Use the title header instead of the separate button.
+	}
 	[HarmonyPatch(typeof(WorkerInspector), "BuildUIForComponent")]
 	public class CollapseButtonPatch {
 		//also skip on allowContainer, this is for when a component is opened in an inspector by itself. No point showing or generating there.
@@ -56,20 +59,31 @@ public class InspectorImprovements : ResoniteMod {
 			foreach (var child in recentComponent.Children.Skip(1).ToList()) {
 				child.Parent = expanderContent;
 			}
+			var mainHeaderSlot = headerSlot.Children.First(s => s.GetComponent<ReferenceProxySource>() != null);
+			var mainHeaderButton = mainHeaderSlot.GetComponent<Button>();
 
 			RadiantUI_Constants.SetupEditorStyle(ui);
 			ui.Style.FlexibleWidth = 0f;
 			ui.Style.MinWidth = 40f;
-
-
-			var button = ui.Button(OfficialAssets.Graphics.Icons.Tool.PerpendicularRay);
-			button.Slot.OrderOffset = collaspeOrderOffset.Value;
-			Expander exp = button.Slot.AttachComponent<Expander>();
+			Button? expBtn;
+			switch (collapseStyle.Value) {
+				case CollapseStyle.Line:
+					expBtn = ui.Button(OfficialAssets.Graphics.Icons.Tool.PerpendicularRay);
+					expBtn.Slot.OrderOffset = collaspeOrderOffset.Value;
+					break;
+				case CollapseStyle.Header:
+					expBtn = mainHeaderButton;
+					break;
+				default:
+					expBtn = ui.Button(OfficialAssets.Graphics.Icons.Tool.PerpendicularRay);
+					expBtn.Slot.OrderOffset = collaspeOrderOffset.Value;
+					break;
+			}
+			Expander exp = expBtn.Slot.AttachComponent<Expander>();
 			exp.SectionRoot.Target = expanderContent;
 			exp.IsExpanded = defaultExpanded.Value;
 		}
 	}
 
 
-	}
 }
